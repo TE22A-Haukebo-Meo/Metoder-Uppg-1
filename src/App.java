79cce700569819d079a56d137c3663191343a37d
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-11-20 09:22:03 +0100

    klar me uppg 1

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
+        System.out.println("Cuantos largos radie vill du ha?");
+        Scanner tb = new Scanner(System.in);
+        double radie = tb.nextDouble();
+        System.out.println("Omkretsen är "+omkretsCirkel(radie));
+    }
         static double omkretsCirkel(double radie)
         {
-            sewey
+            double omkrets = 3.14*radie*2;
+            return omkrets;
         }
-    }
 }
