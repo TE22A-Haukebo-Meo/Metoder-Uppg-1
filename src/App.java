0a9cfe97b327deaab90dd20f3f5ed7e5d7979e54
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-11-20 09:12:10 +0100

    Startat uppgift

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        static double omkretsCirkel(double radie)
+        {
+            sewey
+        }
+    }
+}
