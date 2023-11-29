e911c4f17799818510b34aed506920a93308589e
TE22A-Haukebo-Meo <meo.haukebo@elev.ga.ntig.se>
2023-11-29 10:24:48 +0100

    Klar med uppg e

--- src/App.java
+++ src/App.java
@@ -2,15 +2,28 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("HurlånglinjevillDUha?");
+        //System.out.println("HurlånglinjevillDUha?");
         Scanner tb = new Scanner(System.in);
-        int längd = tb.nextInt();
-        ritaLinje(längd);
+        /*int längd = tb.nextInt();
+        ritaLinje(längd);*/
+        System.out.println("Hurbredrektangelvillduha?");
+        int bredd = tb.nextInt();
+        System.out.println("Hurlhögrektangelvillduha?");
+        int höjd =tb.nextInt();
+        ritaRektangel(bredd, höjd);
     }
         static void ritaLinje(int längd)
         {
             for(int i=0 ; i<längd; i++){
                 System.out.print("-");
+            }
+        }
+        static void ritaRektangel(int bredd, int höjd){
+            for(int i=0; i<bredd; i++){
+                System.out.println();
+                for(int a=0; a<höjd; a++){
+                    System.out.print("*");
+                }
             }
         }
 }
